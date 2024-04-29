a731cf19a12804b490a57444623acf2c23842e2f
sooraj12 <soorajshaji33@gmail.com>
2024-04-30 03:27:33 +0530

    added test prompt

--- chains/sql_prompts.py
+++ chains/sql_prompts.py
@@ -70,3 +70,18 @@ PROMPTS = {
     "postgresql": POSTGRESS_PROMPT,
     "sqlite": SQLITE_PROMPT,
 }
+
+
+query_validation_prompt = """Double check the user's {dialect} query for common mistakes, including:
+- Using NOT IN with NULL values
+- Using UNION when UNION ALL should have been used
+- Using BETWEEN for exclusive ranges
+- Data type mismatch in predicates
+- Properly quoting identifiers
+- Using the correct number of arguments for functions
+- Casting to the correct data type
+- Using the proper columns for joins
+
+If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
+
+Output the final SQL query only."""
